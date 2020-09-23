a8f6eeafe6374d993f263f666efef52fe744566e
Atakan <30434423+atkngnll@users.noreply.github.com>
2020-09-23 22:09:59 +0300

    SMASH HİT - REPORTS

--- Assets/Scripts/Kameraman.cs
+++ Assets/Scripts/Kameraman.cs
@@ -5,70 +5,83 @@ using UnityEngine.SceneManagement;
 
 public class Kameraman : MonoBehaviour
 {
-    public float speed = 2;
-    public float incrementFactor = 0.10f;
-
+    public float speed = 1;
+    public float incrementFactor = 0.02f;
     public float spawnHelper = 4.5f;
     public GameObject ball;
     public float ballForce = 700;
-    public static bool camMoving = false;
-    //public GameObject restartButton;
+    public GameObject restartButton;
+    public GameObject quitButton;
 
-    private CharacterController controller;
+    //We use this when we implement UI
+    public static bool camMoving = false;
+
+    private CharacterController cameraChar;
+    //A boolean whose value will be determined by OnTriggerEnter
     private bool collision = false;
     private Camera _cam;
 
+
+    // Use this for initialization
     void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>();
+        cameraChar = gameObject.GetComponent<CharacterController>();
         _cam = GetComponent<Camera>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        Debug.Log("Speed is" + speed);
+        Debug.Log("Speed is " + speed);
 
         float mousePosx = Input.mousePosition.x;
         float mousePosy = Input.mousePosition.y;
 
         Vector3 BallInstantiatePoint = _cam.ScreenToWorldPoint(new Vector3(mousePosx, mousePosy, _cam.nearClipPlane + spawnHelper));
 
+        //This checks if we have collided
         if (!collision && camMoving)
         {
-            controller.Move(Vector3.forward * Time.deltaTime * speed);
+            cameraChar.Move(Vector3.forward * Time.deltaTime * speed);
+            //This is so that the camera's movement will speed up
             speed = speed + incrementFactor;
         }
         else if (collision || !camMoving)
         {
-            controller.Move(Vector2.zero);
+            cameraChar.Move(Vector3.zero);
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && camMoving)
         {
             GameObject ballRigid;
             ballRigid = Instantiate(ball, BallInstantiatePoint, transform.rotation) as GameObject;
             ballRigid.GetComponent<Rigidbody>().AddForce(Vector3.forward * ballForce);
         }
-    }
 
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Glass"))
+        if (other.CompareTag("glass"))
         {
             collision = true;
-            Debug.Log("HELL YEAH!");
+            Debug.Log("Collided with glass!! Man down!!");
             camMoving = false;
-            //restartButton.SetActive(true);
+            restartButton.SetActive(true);
+            quitButton.SetActive(true);
         }
     }
-
     public void StartCam()
     {
         camMoving = !camMoving;
     }
-
-    public void Restart()
+    public void Reset()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Exitting Game");
+        Application.Quit();
     }
 }
